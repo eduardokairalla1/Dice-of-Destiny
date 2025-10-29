00ad3d80fe15108299d54dcb1c36e0de68403979
tomascubeiro <tocubeiro@gmail.com>
2025-10-29 00:08:00 -0300

    Merge pull request #2 from tomascubeiro/tomas/add-base-structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,29 @@
 package src.main.java;
 
+import src.main.java.characters.*;
+import src.main.java.characters.heroes.*;
+import src.main.java.inventory.*;
+import src.main.java.items.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Welcome to Dice of Destiny ===");
+        System.out.print("Enter your hero's name: ");
+        String name = input.nextLine();
+
+        Inventory playerInventory = new Inventory();
+        playerInventory.addItem(new Item("Healing Potion", "Restores 20 HP", EffectType.HEAL, 3));
+
+        Warrior player = new Warrior(name, playerInventory);
+        Enemy goblin = new Enemy("Goblin", 50, 8, 4, 1, new Inventory());
+
+        goblin.getInventory().addItem(new Item("Rusty Sword", "Increases attack power slightly", EffectType.ATTACK_BOOST, 1));
+
+        player.battle(goblin);
+
+        input.close();
     }
 }
--- src/main/java/characters/heroes/Warrior.java
+++ src/main/java/characters/heroes/Warrior.java
@@ -1,5 +1,17 @@
 package src.main.java.characters.heroes;
 
-public class Warrior {
-    
+import src.main.java.characters.Character;
+import src.main.java.inventory.Inventory;
+
+public class Warrior extends Character {
+    public Warrior(String name, Inventory inventory) {
+        super(name, 100, 15, 10, 1, inventory);
+    }
+
+    @Override
+    public void specialAttack(Character enemy) {
+        float damage = this.attackPower * 1.5f - enemy.getDefense();
+        if (damage > 0) enemy.setHealthPoints(enemy.getHealthPoints() - damage);
+        System.out.printf("%s uses POWER STRIKE and deals %.1f damage!%n", this.name, damage);
+    }
 }
