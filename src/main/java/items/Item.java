f42497f5c5b67fc4c349ed21c89d594cbe7e55e8
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-10-30 21:59:15 -0300

    Refactor Item class equals and hashCode methods

--- src/main/java/items/Item.java
+++ src/main/java/items/Item.java
@@ -1,13 +1,21 @@
+/**
+ * Represents an item in the game.
+ */
+
 package src.main.java.items;
 
-import java.util.Objects;
 
+// --- CODE ---
 public class Item implements Comparable<Item>, Cloneable {
+
+    // --- ATTRIBUTES ---
     private String name;
     private String description;
     private EffectType effect;
     private int quantity;
 
+
+    // --- CONSTRUCTORS ---
     public Item(String name, String description, EffectType effect, int quantity) {
         this.name = name;
         this.description = description;
@@ -15,46 +23,108 @@ public class Item implements Comparable<Item>, Cloneable {
         this.quantity = quantity;
     }
 
-    public Item(Item other) {
+    // Copy constructor
+    public Item(Item other) throws IllegalArgumentException {
+
+        if (other == null) {
+            throw new IllegalArgumentException("Cannot copy null Item");
+        }
+
         this.name = other.name;
         this.description = other.description;
         this.effect = other.effect;
         this.quantity = other.quantity;
     }
 
-    public String getName() { return name; }
-    public String getDescription() { return description; }
-    public EffectType getEffect() { return effect; }
-    public int getQuantity() { return quantity; }
 
-    public void increaseQuantity(int amount) { this.quantity += amount; }
-    public void decreaseQuantity(int amount) { this.quantity = Math.max(0, this.quantity - amount); }
+    // --- GETTERS ---
+    public String getName() {
+        return name;
+    }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public EffectType getEffect() {
+        return effect;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+    // --- METHODS ---
+    public void increaseQuantity(int amount) {
+        this.quantity += amount;
+    }
+
+    public void decreaseQuantity(int amount) throws IllegalArgumentException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+
+        this.quantity = Math.max(0, this.quantity - amount);
+    }
+
+
+    // --- OVERRIDDEN METHODS ---
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Item)) return false;
-        Item item = (Item) o;
-        return Objects.equals(name, item.name) && effect == item.effect;
+    public boolean equals(Object obj)
+    {
+        if (obj == this) return true;
+        if (obj == null) return false;
+        if (obj.getClass()!=this.getClass()) return false;
+
+        Item i = (Item)obj;
+
+        if (!i.name.equals(this.name) ||
+            !i.description.equals(this.description) ||
+            i.effect != this.effect)
+            return false;
+
+        return true;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, effect);
+    public int hashCode()
+    {
+        int resp = 1;
+
+        resp = resp * 2 + this.name.hashCode();
+        resp = resp * 3 + this.description.hashCode();
+
+        if (this.effect!=null) resp = resp * 5 + this.effect.hashCode();
+
+        if (resp < 0) resp = -resp;
+
+        return resp;
     }
 
     @Override
     public int compareTo(Item other) {
-        return this.name.compareToIgnoreCase(other.name);
+
+        // Check for same object
+        if (this == other) return 0;
+
+        // Compare by name
+        int result = this.name.compareToIgnoreCase(other.name);
+
+        if (result < 0) return -1;
+        if (result > 0) return 1;
+
+        return 0;
     }
 
     @Override
-    public Item clone() {
+    public Object clone() {
         return new Item(this);
     }
 
     @Override
-    public String toString() {
-        return name + " (" + description + ") x" + quantity;
+    public String toString()
+    {
+        return this.name + " (" + this.description + ") x" + this.quantity;
     }
 }
